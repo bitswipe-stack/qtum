7817a7fb39e3b246705b793619ee5f6a857f1a45
timemarkovqtum <time.markov@qtum.com>
2025-08-20 09:43:21 +0200

    Update ethash to v1.1.0

--- src/eth_client/utils/ethash/scripts/ci/python_build_wheels.sh
+++ src/eth_client/utils/ethash/scripts/ci/python_build_wheels.sh
@@ -7,11 +7,14 @@
 
 set -eo pipefail
 
+ACTIVATE_DIR=bin
+
 if [ -n "$APPVEYOR" ]; then
-    PYTHON_PATHS="/c/Python310-x64:/c/Python310-x64/Scripts /c/Python39-x64:/c/Python39-x64/Scripts /c/Python38-x64:/c/Python38-x64/Scripts /c/Python37-x64:/c/Python37-x64/Scripts"
+    ACTIVATE_DIR=Scripts
+    PYTHON_PATHS="/c/Python313-x64:/c/Python313-x64/Scripts /c/Python312-x64:/c/Python312-x64/Scripts /c/Python311-x64:/c/Python311-x64/Scripts /c/Python310-x64:/c/Python310-x64/Scripts"
 elif [ -n "$CIRCLECI" ]; then
     if [ "$OSTYPE" = "linux-gnu" ]; then
-        PYTHON_PATHS="/opt/python/cp310-cp310/bin /opt/python/cp39-cp39/bin /opt/python/cp38-cp38/bin /opt/python/cp37-cp37m/bin /opt/python/cp36-cp36m/bin"
+        PYTHON_PATHS="/opt/python/cp313-cp313/bin /opt/python/cp312-cp312/bin /opt/python/cp311-cp311/bin /opt/python/cp310-cp310/bin"
     else
         # The continuation of the script expects "python" executable name,
         # so make link python -> python3.
@@ -25,12 +28,15 @@ PATH_ORIG=$PATH
 for p in $PYTHON_PATHS
 do
     PATH="$p:$PATH_ORIG"
+    python -m venv venv
+    source venv/$ACTIVATE_DIR/activate
     echo '***'
     python --version
     which python
-    python -m pip --version
+    pip --version
     echo '***'
-    python -m pip install wheel
-    python setup.py build_ext --skip-cmake-build
-    python setup.py bdist_wheel --skip-build
+    pip install --use-pep517 build
+    python -m build
+    deactivate
+    rm -rf venv
 done
