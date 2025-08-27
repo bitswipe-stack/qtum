c0fc3108bcc886e179370691156d3c80d3810f98
timemarkovqtum <time.markov@qtum.com>
2025-08-27 16:25:38 +0200

    Port qtumtests unit tests

--- src/test/qtumtests/qtumtxconverter_tests.cpp
+++ src/test/qtumtests/qtumtxconverter_tests.cpp
@@ -53,7 +53,7 @@ void runTest(Chainstate& chainstate, CTxMemPool& mempool, bool isCreation, size_
     std::vector<CTxOut> outs1 = {CTxOut(value, CScript() << OP_DUP << OP_HASH160 << address << OP_EQUALVERIFY << OP_CHECKSIG)};
     tx1 = createTX(outs1);
     uint256 hashParentTx = tx1.GetHash(); // save this txid for later use
-    mempool.addUnchecked(entry.Fee(1000).Time(Now<NodeSeconds>()).SpendsCoinbase(true).FromTx(tx1));
+    AddToMempool(mempool, entry.Fee(1000).Time(Now<NodeSeconds>()).SpendsCoinbase(true).FromTx(tx1));
     std::vector<CTxOut> outs2;
     for(size_t i = 0; i < n; i++){
         if(script2 == CScript()){
@@ -88,7 +88,7 @@ void runFailingTest(Chainstate& chainstate, CTxMemPool& mempool, bool isCreation
     std::vector<CTxOut> outs1 = {CTxOut(value, CScript() << OP_DUP << OP_HASH160 << address << OP_EQUALVERIFY << OP_CHECKSIG)};
     tx1 = createTX(outs1);
     uint256 hashParentTx = tx1.GetHash(); // save this txid for later use
-    mempool.addUnchecked(entry.Fee(1000).Time(Now<NodeSeconds>()).SpendsCoinbase(true).FromTx(tx1));
+    AddToMempool(mempool, entry.Fee(1000).Time(Now<NodeSeconds>()).SpendsCoinbase(true).FromTx(tx1));
     std::vector<CTxOut> outs2;
     for(size_t i = 0; i < n; i++){
         if(script2 == CScript()){
