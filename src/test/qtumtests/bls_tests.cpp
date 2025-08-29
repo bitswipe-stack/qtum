0923e3e8f30ddc81f5127a3900b96d53917f3257
timemarkovqtum <time.markov@qtum.com>
2025-08-29 16:36:47 +0200

    Add tests for add_G2 and msm_G2 bls12

--- src/test/qtumtests/bls_tests.cpp
+++ src/test/qtumtests/bls_tests.cpp
@@ -9,6 +9,12 @@
 #include <test/qtumtests/data/fail_mul_G1_bls.json.h>
 #include <test/qtumtests/data/msm_G1_bls.json.h>
 #include <test/qtumtests/data/fail_msm_G1_bls.json.h>
+#include <test/qtumtests/data/add_G2_bls.json.h>
+#include <test/qtumtests/data/fail_add_G2_bls.json.h>
+#include <test/qtumtests/data/mul_G2_bls.json.h>
+#include <test/qtumtests/data/fail_mul_G2_bls.json.h>
+#include <test/qtumtests/data/msm_G2_bls.json.h>
+#include <test/qtumtests/data/fail_msm_G2_bls.json.h>
 
 #include <span>
 
@@ -56,6 +62,48 @@ BOOST_AUTO_TEST_CASE(checking_fail_msm_G1_bls){
     dev::eth::ChainOperationParams const& params = globalSealEngine->chainParams();
     dev::u256 blockNumber = m_node.chainman->ActiveChain().Tip()->nHeight;
     RunPrecompiledTests(msm_G1_bls, fail_msm_G1_bls, params, blockNumber);
+}
+
+BOOST_AUTO_TEST_CASE(checking_add_G2_bls){
+    // Call add_G2_bls 0xd
+    dev::eth::ChainOperationParams const& params = globalSealEngine->chainParams();
+    dev::u256 blockNumber = m_node.chainman->ActiveChain().Tip()->nHeight;
+    RunPrecompiledTests(add_G2_bls, add_G2_bls, params, blockNumber);
+}
+
+BOOST_AUTO_TEST_CASE(checking_fail_add_G2_bls){
+    // Call add_G2_bls 0xd
+    dev::eth::ChainOperationParams const& params = globalSealEngine->chainParams();
+    dev::u256 blockNumber = m_node.chainman->ActiveChain().Tip()->nHeight;
+    RunPrecompiledTests(add_G2_bls, fail_add_G2_bls, params, blockNumber);
+}
+
+BOOST_AUTO_TEST_CASE(checking_mul_G2_bls){
+    // Call msm_G2_bls 0xe
+    dev::eth::ChainOperationParams const& params = globalSealEngine->chainParams();
+    dev::u256 blockNumber = m_node.chainman->ActiveChain().Tip()->nHeight;
+    RunPrecompiledTests(msm_G2_bls, mul_G2_bls, params, blockNumber);
+}
+
+BOOST_AUTO_TEST_CASE(checking_fail_mul_G2_bls){
+    // Call msm_G2_bls 0xe
+    dev::eth::ChainOperationParams const& params = globalSealEngine->chainParams();
+    dev::u256 blockNumber = m_node.chainman->ActiveChain().Tip()->nHeight;
+    RunPrecompiledTests(msm_G2_bls, fail_mul_G2_bls, params, blockNumber);
+}
+
+BOOST_AUTO_TEST_CASE(checking_msm_G2_bls){
+    // Call msm_G2_bls 0xe
+    dev::eth::ChainOperationParams const& params = globalSealEngine->chainParams();
+    dev::u256 blockNumber = m_node.chainman->ActiveChain().Tip()->nHeight;
+    RunPrecompiledTests(msm_G2_bls, msm_G2_bls, params, blockNumber);
+}
+
+BOOST_AUTO_TEST_CASE(checking_fail_msm_G2_bls){
+    // Call msm_G2_bls 0xe
+    dev::eth::ChainOperationParams const& params = globalSealEngine->chainParams();
+    dev::u256 blockNumber = m_node.chainman->ActiveChain().Tip()->nHeight;
+    RunPrecompiledTests(msm_G2_bls, fail_msm_G2_bls, params, blockNumber);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
