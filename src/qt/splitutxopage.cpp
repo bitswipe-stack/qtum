0655b51b4d509b3f9b5ac2097ff72c6dba3dce68
timemarkovqtum <time.markov@qtum.com>
2025-09-23 14:11:36 +0200

    Fix splitutxopage default max outputs value

--- src/qt/splitutxopage.cpp
+++ src/qt/splitutxopage.cpp
@@ -69,7 +69,7 @@ SplitUTXOPage::SplitUTXOPage(QWidget *parent, Mode mode) :
     ui->lineEditMaxValue->setValue(DEFAULT_STAKING_MIN_UTXO_VALUE * 2);
     ui->spinBoxMaxOutputs->setMinimum(1);
     ui->spinBoxMaxOutputs->setMaximum(10000);
-    ui->spinBoxMaxOutputs->setValue(100);
+    setDefaultMaxOutputsValue();
 
     ui->splitCoinsButton->setEnabled(false);
 
@@ -118,10 +118,7 @@ void SplitUTXOPage::setModel(WalletModel *_model)
         ui->splitCoinsButton->setToolTip(tr("Creates a Partially Signed Qtum Transaction (PSBT) for use with e.g. an offline %1 wallet, or a PSBT-compatible hardware wallet.").arg(CLIENT_NAME));
     }
 
-    if(m_model && m_model->wallet().privateKeysDisabled())
-    {
-        ui->spinBoxMaxOutputs->setValue(20);
-    }
+    setDefaultMaxOutputsValue();
 }
 
 void SplitUTXOPage::setAddress(const QString &address)
@@ -176,7 +173,7 @@ void SplitUTXOPage::clearAll()
     }
     ui->lineEditMinValue->setValue(DEFAULT_STAKING_MIN_UTXO_VALUE);
     ui->lineEditMaxValue->setValue(DEFAULT_STAKING_MIN_UTXO_VALUE * 2);
-    ui->spinBoxMaxOutputs->setValue(100);
+    setDefaultMaxOutputsValue();
 }
 
 void SplitUTXOPage::accept()
@@ -315,4 +312,10 @@ void SplitUTXOPage::on_updateSplitCoinsButton()
     }
 
     ui->splitCoinsButton->setEnabled(enabled);
+}
+
+void SplitUTXOPage::setDefaultMaxOutputsValue()
+{
+    uint8_t numOutputs = m_model && m_model->wallet().privateKeysDisabled() ? 20 : 100;
+    ui->spinBoxMaxOutputs->setValue(numOutputs);
 }
